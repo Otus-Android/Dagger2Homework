309cc343934bc5875438935f3cf8aba1878934a5
Elisey Lobanov <crimsonorama@gmail.com>
2022-05-20 19:20:54 +0300

    Dagger Homework

--- app/src/main/java/ru/otus/daggerhomework/App.kt
+++ app/src/main/java/ru/otus/daggerhomework/App.kt
@@ -1,6 +1,19 @@
 package ru.otus.daggerhomework
 
 import android.app.Application
+import android.content.Context
 
-class App :Application() {
+class App: Application() {
+    lateinit var applicationComponent: ApplicationComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        applicationComponent = DaggerApplicationComponent.factory().create(applicationContext)
+    }
 }
+
+val Context.applicationComponent: ApplicationComponent
+    get() = when (this)  {
+        is App -> applicationComponent
+        else -> this.applicationContext.applicationComponent
+    }
