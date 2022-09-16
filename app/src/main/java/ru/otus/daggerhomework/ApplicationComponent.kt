03e442dd1ba71dc23fb874204bb7d36e5ff8d873
D_Mironov_29682 <d.mironov@rendez-vous.ru>
2022-09-16 19:47:35 +0300

    created new components

--- app/src/main/java/ru/otus/daggerhomework/ApplicationComponent.kt
+++ app/src/main/java/ru/otus/daggerhomework/ApplicationComponent.kt
@@ -1,4 +1,27 @@
 package ru.otus.daggerhomework
 
+import android.content.Context
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Qualifier
+import javax.inject.Singleton
+
+@Qualifier
+annotation class QualifierAppContext
+
+@Singleton
+@Component
 interface ApplicationComponent {
+
+    @QualifierAppContext
+    fun provideAppContext(): Context
+
+    @Component.Factory
+    interface AppComponentFactory {
+        fun create(
+            @QualifierAppContext
+            @BindsInstance
+            applicationContext: Context
+        ): ApplicationComponent
+    }
 }
