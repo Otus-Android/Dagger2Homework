d419d27edeb7dae22f739f9ca9a4e63013156a0e
V <petrov.vladislav1992@gmail.com>
2023-02-08 17:40:43 +0600

    Sync

---
+++ app/src/main/java/ru/otus/daggerhomework/di/ApplicationComponent.kt
@@ -0,0 +1,12 @@
+package ru.otus.daggerhomework.di
+
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component
+interface ApplicationComponent {
+
+    //fun applicationContext
+
+}
