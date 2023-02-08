fc6d4ec29560f5f52f962cffc881754168e51d0b
Kamil Tayupov <k.i.tayupov@mail.ru>
2023-02-08 20:44:19 +0300

    Init Application Component

--- app/src/main/java/ru/otus/daggerhomework/App.kt
+++ app/src/main/java/ru/otus/daggerhomework/App.kt
@@ -2,5 +2,12 @@ package ru.otus.daggerhomework
 
 import android.app.Application
 
-class App :Application() {
+class App : Application() {
+
+  lateinit var applicationComponent: ApplicationComponent
+
+  override fun onCreate() {
+    super.onCreate()
+    applicationComponent = DaggerApplicationComponent.factory().create(this)
+  }
 }
--- app/src/main/java/ru/otus/daggerhomework/ApplicationComponent.kt
+++ app/src/main/java/ru/otus/daggerhomework/ApplicationComponent.kt
@@ -1,4 +1,16 @@
 package ru.otus.daggerhomework
 
+import android.content.Context
+import dagger.BindsInstance
+import dagger.Component
+
+@Component
 interface ApplicationComponent {
+
+  fun context(): Context
+
+  @Component.Factory
+  interface Factory {
+    fun create(@BindsInstance context: Context): ApplicationComponent
+  }
 }
