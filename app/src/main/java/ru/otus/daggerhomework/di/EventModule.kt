2246e22f23ddac812a06225dd8a6c79d066bcf2a
Olga Teplitskaia <zxlnk@yandex.com>
2023-05-15 21:19:48 +0300

    module with bind and provides

--- app/src/main/java/ru/otus/daggerhomework/di/EventModule.kt
+++ app/src/main/java/ru/otus/daggerhomework/di/EventModule.kt
@@ -1,9 +1,11 @@
 package ru.otus.daggerhomework.di
 
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import ru.otus.daggerhomework.AppEvent
 import ru.otus.daggerhomework.ViewModelReceiver
@@ -14,11 +16,13 @@ import javax.inject.Singleton
  * которая живет дольше, MainActivity и MainActivityComponent
  */
 @Module
-object EventModule {
-    @Singleton
-    @Provides
-    fun provideMutableEventFlow() = MutableSharedFlow<AppEvent>(0, 1, BufferOverflow.DROP_OLDEST)
+interface EventModule {
+    companion object {
+        @Singleton
+        @Provides
+        fun provideMutableEventFlow() = MutableSharedFlow<AppEvent>(0, 1, BufferOverflow.DROP_OLDEST)
+    }
 
-    @Provides
-    fun provideEventFlow(eventFlow: MutableSharedFlow<AppEvent>) = eventFlow.asSharedFlow()
+    @Binds
+    fun bindEventFlow(impl: MutableSharedFlow<AppEvent>): SharedFlow<AppEvent>
 }
