7a07bfbdbc0361bde8d966e3f97cc902b4b7eca5
RadSham <radsham9@gmail.com>
2023-11-26 18:14:04 +0300

    HWDagger2. refactor

--- app/src/main/java/ru/otus/daggerhomework/App.kt
+++ app/src/main/java/ru/otus/daggerhomework/App.kt
@@ -1,11 +1,9 @@
 package ru.otus.daggerhomework
 
 import android.app.Application
-import javax.inject.Inject
 
 class App : Application() {
 
-    @Inject
     lateinit var applicationComponent: ApplicationComponent
 
     override fun onCreate() {
