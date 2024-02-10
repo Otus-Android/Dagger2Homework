a1be2a6f2f46d211185e01dcb9abfe6d6bd977a8
Alexander <alex.zacepin@gmail.com>
2024-02-10 21:14:42 +0100

    new version

--- app/src/main/java/ru/otus/daggerhomework/FragmentReceiver.kt
+++ app/src/main/java/ru/otus/daggerhomework/FragmentReceiver.kt
@@ -1,16 +1,34 @@
 package ru.otus.daggerhomework
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.annotation.ColorInt
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 class FragmentReceiver : Fragment() {
 
+    @Inject
+    lateinit var viewModelReceiver: ViewModelReceiver
+
     private lateinit var frame: View
+    private var oldColor = 0
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        DaggerViewModelReceiverComponent
+            .factory()
+            .create(context.applicationContext)
+            .inject(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,6 +41,23 @@ class FragmentReceiver : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         frame = view.findViewById(R.id.frame)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                while (true) {
+                    delay(50)
+                    launch {
+                        viewModelReceiver.observeColors().collect {
+                            if (oldColor != it) {
+                                populateColor(it)
+                                oldColor = it
+                                viewModelReceiver.showToast = true
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
     }
 
     fun populateColor(@ColorInt color: Int) {
--- app/src/main/java/ru/otus/daggerhomework/ViewModelReceiver.kt
+++ app/src/main/java/ru/otus/daggerhomework/ViewModelReceiver.kt
@@ -3,13 +3,57 @@ package ru.otus.daggerhomework
 import android.app.Application
 import android.content.Context
 import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.Binds
+import dagger.BindsInstance
+import dagger.Component
+import dagger.Module
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
 
-class ViewModelReceiver(
-    private val context: Context
-) {
-
-    fun observeColors() {
+class ViewModelReceiver @Inject constructor(
+    private val context: Context,
+    private val colorGenerator: ColorGenerator
+) : ViewModel() {
+    var showToast = true
+    suspend fun observeColors(): StateFlow<Int> {
         if (context !is Application) throw RuntimeException("Здесь нужен контекст апликейшена")
-        Toast.makeText(context, "Color received", Toast.LENGTH_LONG).show()
+        if(showToast){
+            Toast.makeText(context, "Color received", Toast.LENGTH_SHORT).show()
+            showToast = false}
+        return colorGenerator.getColor().stateIn(CoroutineScope(Dispatchers.IO))
     }
+}
+
+class ViewModelReceiverFactory @Inject constructor(
+    private val  context: Context,
+    private val colorGenerator: ColorGenerator
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return ViewModelReceiver( context, colorGenerator) as T
+    }
+}
+
+@Component( modules = [ViewModelReceiverModule::class, ColorGeneratorModule::class]
+)
+interface ViewModelReceiverComponent {
+
+    @Component.Factory
+    interface Factory {
+        fun create(
+            @BindsInstance context: Context,
+        ): ViewModelReceiverComponent
+    }
+
+    fun inject(fragmentReceiver: FragmentReceiver)
+}
+
+@Module
+interface ViewModelReceiverModule {
+    @Binds
+    fun bindFactory(viewModelReceiverFactory: ViewModelReceiverFactory): ViewModelProvider.Factory
 }
