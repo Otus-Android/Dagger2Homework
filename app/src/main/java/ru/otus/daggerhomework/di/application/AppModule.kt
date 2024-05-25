88f0b97904871ce52a540465cf394fa8289da0a9
dmturov <dmturov@ozon.ru>
2024-05-25 21:38:38 +0300

    refactor

--- app/src/main/java/ru/otus/daggerhomework/di/application/AppModule.kt
+++ app/src/main/java/ru/otus/daggerhomework/di/application/AppModule.kt
@@ -1,6 +1,7 @@
 package ru.otus.daggerhomework.di.application
 
 import android.content.Context
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import ru.otus.daggerhomework.ColorGenerator
@@ -8,17 +9,17 @@ import ru.otus.daggerhomework.ColorGeneratorImpl
 import ru.otus.daggerhomework.di.annotation.Application
 
 @Module
-object AppModule {
-    @Application
-    @Provides
-    @JvmStatic
-    fun provideApplicationContext(context: Context): Context {
-        return context
+interface AppModule {
+
+    companion object {
+        @Application
+        @Provides
+        @JvmStatic
+        fun provideApplicationContext(context: Context): Context {
+            return context
+        }
     }
 
-    @Provides
-    @JvmStatic
-    fun provideColorGenerator(): ColorGenerator {
-        return ColorGeneratorImpl()
-    }
+    @Binds
+    fun bindColorGenerator(colorGeneratorImpl: ColorGeneratorImpl): ColorGenerator
 }
