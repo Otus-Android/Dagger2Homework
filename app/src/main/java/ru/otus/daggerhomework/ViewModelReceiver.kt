2b457c4e8ef0b88e7cc79acd6602bb8241531fe8
Egor Glukhov <e.glukhov@mayber.dev>
2024-06-15 21:39:29 +0600

    StateFlow

--- app/src/main/java/ru/otus/daggerhomework/ViewModelReceiver.kt
+++ app/src/main/java/ru/otus/daggerhomework/ViewModelReceiver.kt
@@ -6,16 +6,13 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.observeOn
-import kotlinx.coroutines.flow.subscribe
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class ViewModelReceiver(
     private val context: Context,
-    private val observer: MutableStateFlow<Int>,
+    private val observer: StateFlow<Int>,
     private val onReceive: (Int) -> Unit,
 ): ViewModel() {
 
