cc6233fdbe838e551189c34b5b246ccc394ecc80
vrazdyakonov <razdyakonov.viktor@gmail.com>
2024-07-13 11:02:58 +0300

    Remove @Retention(RUNTIME) from ActivityContext and AppContext qualifiers

--- app/src/main/java/ru/otus/daggerhomework/qualifiers/ActivityContext.kt
+++ app/src/main/java/ru/otus/daggerhomework/qualifiers/ActivityContext.kt
@@ -1,8 +1,6 @@
 package ru.otus.daggerhomework.qualifiers
 
 import javax.inject.Qualifier
-import kotlin.annotation.AnnotationRetention.RUNTIME
 
 @Qualifier
-@Retention(RUNTIME)
 annotation class ActivityContext
