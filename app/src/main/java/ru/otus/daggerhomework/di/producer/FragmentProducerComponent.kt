e6dadf38d9d8d3853bd92b1f0fae10ee91a602a5
Ponyu-dev <mopoypa@yandex.ru>
2024-07-31 19:51:10 +0300

    Настроил FragmentProducer

--- app/src/main/java/ru/otus/daggerhomework/di/producer/FragmentProducerComponent.kt
+++ app/src/main/java/ru/otus/daggerhomework/di/producer/FragmentProducerComponent.kt
@@ -1,7 +1,7 @@
 package ru.otus.daggerhomework.di.producer
 
 import dagger.Subcomponent
-import ru.otus.daggerhomework.FragmentProducer
+import ru.otus.daggerhomework.presentation.producer.FragmentProducer
 import ru.otus.daggerhomework.di.utils.scopes.FragmentScope
 
 @FragmentScope
