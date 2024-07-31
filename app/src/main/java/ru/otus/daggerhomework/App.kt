38ac51fac5e47cbefd46f6d3dab6902912e60f8a
Ponyu-dev <mopoypa@yandex.ru>
2024-07-31 19:38:37 +0300

    Реализовал ApplicationComponent

--- app/src/main/java/ru/otus/daggerhomework/App.kt
+++ app/src/main/java/ru/otus/daggerhomework/App.kt
@@ -1,6 +1,21 @@
 package ru.otus.daggerhomework
 
 import android.app.Application
+import android.content.Context
+import ru.otus.daggerhomework.di.ApplicationComponent
+import ru.otus.daggerhomework.di.DaggerApplicationComponent
 
 class App :Application() {
+
+    private lateinit var applicationComponent: ApplicationComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        applicationComponent = DaggerApplicationComponent.factory().create(this)
+    }
+
+    companion object {
+        fun appComponent(context: Context) =
+            (context.applicationContext as App).applicationComponent
+    }
 }
