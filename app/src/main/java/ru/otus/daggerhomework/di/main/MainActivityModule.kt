23fbb6405a71d779a482b4eb83fa9bfb3418d018
Ponyu-dev <mopoypa@yandex.ru>
2024-07-31 19:36:07 +0300

    Создал MainActivitySubcomponentsModule и MainActivityModule и MainActivityComponent

---
+++ app/src/main/java/ru/otus/daggerhomework/di/main/MainActivityModule.kt
@@ -0,0 +1,21 @@
+package ru.otus.daggerhomework.di.main
+
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import kotlinx.coroutines.channels.Channel
+import ru.otus.daggerhomework.ColorGenerator
+import ru.otus.daggerhomework.ColorGeneratorImpl
+import ru.otus.daggerhomework.di.utils.scopes.ActivityScope
+
+@Module
+interface MainActivityModule {
+    @Binds
+    fun bindColorGenerator(colorGenerator: ColorGeneratorImpl): ColorGenerator
+
+    companion object {
+        @ActivityScope
+        @Provides
+        fun provideChannel(): Channel<Int> = Channel(Channel.CONFLATED)
+    }
+}
