028f921d36524205628210fa78ed2390d2392dab
bmv-2143 <bmv-2143@yandex.ru>
2024-08-09 17:31:34 +0300

    feat: add FragmentReceiverComponent

--- app/src/main/java/ru/otus/daggerhomework/FragmentReceiver.kt
+++ app/src/main/java/ru/otus/daggerhomework/FragmentReceiver.kt
@@ -1,31 +1,44 @@
 package ru.otus.daggerhomework
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.annotation.ColorInt
 import androidx.fragment.app.Fragment
+import javax.inject.Inject
 
 class FragmentReceiver : Fragment() {
 
     private lateinit var frame: View
+
+    @Inject
+    lateinit var viewModelReceiver: ViewModelReceiver
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        (requireActivity() as MainActivity).mainActivityComponent.receiverComponent()
+            .create().inject(this)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_b, container, true)
+        return inflater.inflate(R.layout.fragment_b, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         frame = view.findViewById(R.id.frame)
+        viewModelReceiver.observeColors { color ->
+            populateColor(color)
+        }
     }
 
-    fun populateColor(@ColorInt color: Int) {
+    private fun populateColor(@ColorInt color: Int) {
         frame.setBackgroundColor(color)
     }
 }
---
+++ app/src/main/java/ru/otus/daggerhomework/FragmentReceiverComponent.kt
@@ -0,0 +1,14 @@
+package ru.otus.daggerhomework
+
+import dagger.Subcomponent
+
+@Subcomponent
+interface FragmentReceiverComponent {
+
+    @Subcomponent.Factory
+    interface Factory {
+        fun create(): FragmentReceiverComponent
+    }
+
+    fun inject(receiverFragment: FragmentReceiver)
+}
